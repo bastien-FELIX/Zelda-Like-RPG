629bf205bd484841db0b07ebe02e7ee3222207eb
triton-lrdf <malik.jabou@etu.univ-lyon1.fr>
2025-07-14 11:15:34 +0100

    j'ai repris ajd je v enfin coder un peu

---
+++ code/en-tete/item.h
@@ -0,0 +1,23 @@
+
+
+
+class  Item {
+
+private:
+    
+
+public:
+
+    // un item peut etre affiché 
+    // que ce soit dans l'inventaire ou en tant que loot au sol
+    // on aura d'autre truc mais pour l'instant c deja pas mal
+
+};
+
+
+
+
+
+
+
+
